86ff9dbc8ad645639bc34d5d3ec844bbe0302f1d
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-21 23:07:51 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-me = {"name" => "Kyle", "age" => 27, "job" => "Microbiologist"
+me = {"name" => "Kyle", "age" => 27, "job" => "Microbiologist"}
   
 end
 
